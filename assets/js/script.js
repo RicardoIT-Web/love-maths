0cca2af74c94c8383032aecdf66fc128db17ee66
RicardoIT-Web <rick_piedade@hotmail.com>
2021-09-20 17:20:37 +0000

    Add faviicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    runGame("addition")
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
+    runGame("addition");
+
 });
 
 /**
@@ -23,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
